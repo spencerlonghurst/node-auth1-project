1701174a39fb9791840e4b2f5daa11077d62e648
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-25 21:12:32 -0600

    Built out server, middleware and skeleton of routers

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model');
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +8,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (!req.session.user) {
+    next({ status: 401, message: 'You shall not pass!'})
+  } else {
+    next()
+  }
 }
 
 /*
@@ -18,8 +24,17 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if (!users.length) {
+      next()
+    } else {
+      next({ status: 422, message: 'Username taken' })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -30,8 +45,17 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if (!users.length) {
+      next()
+    } else {
+      next({ status: 401, message: 'Invalid credentials' })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -42,8 +66,19 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body
+  if (!password || password.length < 3) {
+    next({ status: 422, message: 'Password must be longer than 3 chars' })
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
